3677677201f2e83aaea9fcce7dbb0173c0a98bc8
ramkumar-1819 <rohitram1819@gmail.com>
2021-03-30 14:37:10 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Home from './Components/Home/Home';
+import Form from './Components/Form/Form';
+import List from './Components/List/List';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
-function App() {
+import React from 'react';
+
+class  App extends React.Component {
+  constructor(){
+    super();
+    this.state={details:[0,0,0,0],
+                form_detail:[]
+              }
+  }
+  handle=(val,arr)=>{
+    this.setState({details:val,form_detail:arr})
+  }
+  render(){
+    console.log(this.state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    <Router>
+      <Navigation/>
+      <Switch>
+        <Route exact path="/">
+          <div className="section">
+          <Home details={this.state.details}/>
+          </div>
+        </Route>
+        <Route path="/add">
+          <div className="section">
+            <Form details={this.state.details} form_detail={this.state.form_detail} handle={this.handle}/>
+          </div>
+        </Route>
+        <Route path='/list'>
+          <div className="section">
+            <List form_detail={this.state.form_detail}/>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
+}
 }
 
 export default App;
